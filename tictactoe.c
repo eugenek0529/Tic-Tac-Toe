2f926fcdd1399aa3b3df38b38b09abb386ae3a96
Eugene Kim <eugenek0529@gmail.com>
2023-08-01 13:27:24 -0700

    displayBoard and resetBoard created

--- tictactoe.c
+++ tictactoe.c
@@ -3,6 +3,9 @@
     Program: tictactoe.c
     Programmer: Eugene Kim
     About: 
+        * User 1 = main player
+        * User 2 = other end player
+        * Computer player
         * First is to create a structure of game tic-tac-toe
             * mode 1 User vs Computer
             * mode 2 User vs User via same keyboard
@@ -14,6 +17,7 @@
 */
 #include <stdio.h> 
 #include <time.h>
+#include <stdlib.h>
 
 
 // function prototypes
@@ -35,8 +39,64 @@ const char X ='X';
 int main()
 {
 
+    // SEED RNG
+    srand(time(0));
+
+    //variables
+    int keepPlay = 0; // 0 = stop, 1 = continue
+
+
+    /*
+        Outer do-while loop with listing menu
+        inner do-while loop is continuous game play until other mode or exit
+
+    */
+
+    printf("Welcome to the Tic-Tac-Toe!\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf(".");
+        delay(1000);
+    }
+    printf("\n");
+
+
+    do{
+        // Every enter here is new start
+        int status = 0; // track the game status
+        int 
+
+        do{
+
+        }
+
+    } while (keepPlay == 1);
+
 
 
 
     return 0;
+}
+
+
+void displayBoard()
+{
+    printf("+-----------+\n");
+    printf("| %c | %c | %c |\n", board[0][0],board[0][1],board[0][2]);
+    printf("+-----------+\n");
+    printf("| %c | %c | %c |\n", board[1][0],board[1][1],board[1][2]);
+    printf("+-----------+\n");
+    printf("| %c | %c | %c |\n", board[2][0],board[2][1],board[2][2]);
+    printf("+-----------+\n");
+}
+
+void resetBoard()
+{
+    for(int i = 0; i < 3; ++i)
+    {
+        for(int j = 0; j < 3; ++j)
+        {
+            board[i][j] = ' ';
+        }
+    }
 }
