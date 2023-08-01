9386a1ee1825bc3b011345c243d5adddba50db77
Eugene Kim <eugenek0529@gmail.com>
2023-07-31 23:32:21 -0700

    Init commit with basic structure comments in tictactoe.c

---
+++ tictactoe.c
@@ -0,0 +1,32 @@
+/*
+
+    Program: tictactoe.c
+    Programmer: Eugene Kim
+    About: 
+        * First is to create a structure of game tic-tac-toe
+            * mode 1 User vs Computer
+            * mode 2 User vs User via same keyboard
+            * mode 3 user vs user via MQTT
+        * Then Use MQTT to communicate with other end user to 
+           play game together
+
+
+*/
+#include <stdio.h> 
+#include <time.h>
+
+
+// function prototypes
+
+
+// global variables
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
