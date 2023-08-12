8bb9a290f6c04725e194c04ee69b74c54ffff057
Eugene Kim <eugenek0529@gmail.com>
2023-08-11 19:08:20 -0700

    checKWinner modified

--- tictactoe.c
+++ tictactoe.c
@@ -198,72 +198,58 @@ int isFull()
 
 int checkWinner()
 {
-    // initially 0 = progressing
-    int result = 0; 
-    int check; 
+    // 0 = continue play, 1 = yes winner, -1 = tie
 
-    if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]) && board[0][0] != ' ') 
+    if((board[0] == board[1]) && (board[0] == board[2]) && (board[0] != ' '))
     {
-        // Column check start
-        winner = board[0][0]; // store winning character 
+        winner = board[0];
         return 1;
     }
-    else if((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && board[1][0] != ' ') 
+    else if((board[3] == board[4]) && (board[3] == board[5]) && (board[3] != ' '))
     {
-        winner = board[1][0];
-        return 1;    
+        winner = board[3];
+        return 1;
     }
-    else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && board[2][0] != ' ')
+    else if((board[6] == board[7]) && (board[6] == board[8]) && (board[6] != ' '))
     {
-        winner = board[2][0];
-        return 1; 
-    } 
-    else if((board[0][0] == board[1][0]) && (board[1][0] == board[2][0]) && board[0][0] != ' ')
-    {
-        // ROW check start
-        winner = board[0][0];
-        return 1; 
+        winner = board[6];
+        return 1;
     }
-    else if((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]) && board[0][1] != ' ')
+    else if((board[0] == board[3]) && (board[0] == board[6]) && (board[0] != ' '))
     {
-        winner = board[0][1];
-        return 1; 
+        winner = board[0];
+        return 1;
     }
-    else if((board[0][2] == board[1][2]) && (board[1][2] == board[2][2]) && board[0][2] != ' ')
+    else if((board[1] == board[4]) && (board[1] == board[7]) && (board[1] != ' '))
     {
-        winner = board[0][2];
-        return 1; 
+        winner = board[1];
+        return 1;
     }
-    else if((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[0][0] != ' ')
+    else if((board[2] == board[5]) && (board[2] == board[8]) && (board[2] != ' '))
     {
-        // diagnal check start
-        winner = board[0][0];
-        return 1; 
+        winner = board[2];
+        return 1;
     }
-    else if((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && board[0][2] != ' ')
+    else if((board[0] == board[4]) && (board[0] == board[8]) && (board[0] != ' '))
     {
-        winner = board[0][2];
-        return 1; 
+        winner = board[0];
+        return 1;
+    }
+    else if((board[2] == board[4]) && (board[2] == board[6]) && (board[2] != ' '))
+    {
+        winner = board[3];
+        return 1;
     }
     else
     {
-        check = isFull(); 
-
-        //enter here if no match found
-        if(check == 1)
+        int full = 0;
+        full = isFull();
+        if(full == 1)
         {
-            //enter here when board is full, but no matching winner found
-            return -1; // tie
+            // board is full but no winner == tie
+            return -1; 
         }
-        else
-        {
-            return 0; //still progressing
-        }
+        return 0;
 
     }
-
-    return result;
-    
-
-
 }
